df35d621566bd67a81b663dbccbf4b7914f20995
Anton <imladris.doc@gmail.com>
2023-06-08 01:38:24 +0300

    League adapter3 great

--- app/src/main/java/com/example/infocrew/presentation/adapters/ChampionshipAdapter.kt
+++ app/src/main/java/com/example/infocrew/presentation/adapters/ChampionshipAdapter.kt
@@ -3,18 +3,22 @@ package com.example.infocrew.presentation.adapters
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
 import com.example.infocrew.R
 import com.example.infocrew.data.json.league.ChampionshipGroup
 import com.example.infocrew.data.json.league.League
+import com.example.infocrew.data.json.league.Premiership
+import com.example.infocrew.data.json.league.RelegationGroup
 import com.example.infocrew.databinding.LeagueItemBinding
 import com.squareup.picasso.Picasso
 
 class ChampionshipAdapter: ListAdapter<ChampionshipGroup, ChampionshipAdapter.Holder>(Comparator()){
     class Holder(view: View) : RecyclerView.ViewHolder(view){
         val binding = LeagueItemBinding.bind(view)
+        val number: TextView = itemView.findViewById(R.id.tvNumb)
 
         fun bind (item: ChampionshipGroup) = with(binding){
             tvName.text = item.name
@@ -47,6 +51,88 @@ class ChampionshipAdapter: ListAdapter<ChampionshipGroup, ChampionshipAdapter.Ho
 
     override fun onBindViewHolder(holder: Holder, position: Int) {
         holder.bind(getItem(position))
+        holder.number.text = (position + 1).toString()
+
+    }
+}
+class RelegationAdapter: ListAdapter<RelegationGroup, RelegationAdapter.Holder>(Comparator()){
+    class Holder(view: View) : RecyclerView.ViewHolder(view){
+        val binding = LeagueItemBinding.bind(view)
+        val number: TextView = itemView.findViewById(R.id.tvNumb)
+
+        fun bind (item: RelegationGroup) = with(binding){
+            tvName.text = item.name
+            tvP.text = item.p
+            tvW.text = item.w
+            tvD.text = item.d
+            tvL.text = item.l
+            tvPlus.text = item.plus
+            tvP2.text = item.p2
+            Picasso.get().load(item.logo).into(ivLogo)
+        }
+    }
+
+    class Comparator : DiffUtil.ItemCallback<RelegationGroup>(){
+        override fun areItemsTheSame(oldItem: RelegationGroup, newItem: RelegationGroup): Boolean {
+            return  oldItem == newItem
+
+        }
+
+        override fun areContentsTheSame(oldItem: RelegationGroup, newItem: RelegationGroup): Boolean {
+            return oldItem == newItem
+        }
+
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): Holder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.league_item, parent, false)
+        return Holder(view)
+    }
+
+    override fun onBindViewHolder(holder: Holder, position: Int) {
+        holder.bind(getItem(position))
+        holder.number.text = (position + 1).toString()
+
+    }
+}
+
+class PremiershipAdapter: ListAdapter<Premiership, PremiershipAdapter.Holder>(Comparator()){
+    class Holder(view: View) : RecyclerView.ViewHolder(view){
+        val binding = LeagueItemBinding.bind(view)
+        val number: TextView = itemView.findViewById(R.id.tvNumb)
+
+        fun bind (item: Premiership) = with(binding){
+            tvName.text = item.name
+            tvP.text = item.p
+            tvW.text = item.w
+            tvD.text = item.d
+            tvL.text = item.l
+            tvPlus.text = item.plus
+            tvP2.text = item.p2
+            Picasso.get().load(item.logo).into(ivLogo)
+        }
+    }
+
+    class Comparator : DiffUtil.ItemCallback<Premiership>(){
+        override fun areItemsTheSame(oldItem: Premiership, newItem: Premiership): Boolean {
+            return  oldItem == newItem
+
+        }
+
+        override fun areContentsTheSame(oldItem: Premiership, newItem: Premiership): Boolean {
+            return oldItem == newItem
+        }
+
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): Holder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.league_item, parent, false)
+        return Holder(view)
+    }
+
+    override fun onBindViewHolder(holder: Holder, position: Int) {
+        holder.bind(getItem(position))
+        holder.number.text = (position + 1).toString()
 
     }
 }
--- app/src/main/java/com/example/infocrew/presentation/screens/LeagueFragment.kt
+++ app/src/main/java/com/example/infocrew/presentation/screens/LeagueFragment.kt
@@ -9,12 +9,16 @@ import androidx.fragment.app.activityViewModels
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.infocrew.databinding.FragmentLeagueBinding
 import com.example.infocrew.presentation.adapters.ChampionshipAdapter
+import com.example.infocrew.presentation.adapters.PremiershipAdapter
+import com.example.infocrew.presentation.adapters.RelegationAdapter
 import com.example.infocrew.presentation.domain.MainViewModel
 
 class LeagueFragment : Fragment() {
-private lateinit var binding: FragmentLeagueBinding
-private lateinit var adapter: ChampionshipAdapter
-private val model: MainViewModel by activityViewModels()
+    private lateinit var binding: FragmentLeagueBinding
+    private lateinit var adapter: ChampionshipAdapter
+    private val model: MainViewModel by activityViewModels()
+    private lateinit var relegationAdapter : RelegationAdapter
+    private lateinit var premiershipAdapter : PremiershipAdapter
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -27,20 +31,31 @@ private val model: MainViewModel by activityViewModels()
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         initRcView()
+        initRcViewRelegation()
+        initRcViewPremiership()
     }
 
-
+    private fun initRcViewPremiership() = with(binding) {
+        model.liveDataLeague.observe(viewLifecycleOwner){
+            val list2 = it.premiership
+            rv3.layoutManager = LinearLayoutManager(activity)
+            premiershipAdapter = PremiershipAdapter()
+            rv3.adapter = premiershipAdapter
+            premiershipAdapter.submitList(list2)
+        }
+    }
+    private fun initRcViewRelegation() = with(binding) {
+        model.liveDataLeague.observe(viewLifecycleOwner){
+            val list1 = it.relegation_group
+            rv2.layoutManager = LinearLayoutManager(activity)
+            relegationAdapter = RelegationAdapter()
+            rv2.adapter = relegationAdapter
+            relegationAdapter.submitList(list1)
+        }
+    }
     private fun initRcView() = with(binding) {
         model.liveDataLeague.observe(viewLifecycleOwner){
             val list = it.championship_group
-            //Log.d("Recycler Test", it[0].championship_group.toString())
-
-//            val list2 = listOf(
-//                ChampionshipGroup("1",
-//                    "1","1","1","2","2","2","3"
-//                )
-//            )
-
             rv1.layoutManager = LinearLayoutManager(activity)
             adapter = ChampionshipAdapter()
             rv1.adapter = adapter
